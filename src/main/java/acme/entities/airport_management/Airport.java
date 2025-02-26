7edc86af603818485b729e76c58fbd3026af5b9a
Pablo Nieto Córdoba <pabniecor@alum.us.es>
2025-02-26 21:27:54 +0100

    feat: Airport Entity

---
+++ src/main/java/acme/entities/airport_management/Airport.java
@@ -0,0 +1,60 @@
+
+package acme.entities.airport_management;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Airport extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{3}$")
+	@Column(unique = true)
+	private String				IATA;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private OperationalScope	operationalScope;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				cityAndCountry;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				website;
+
+	@Optional
+	@ValidEmail
+	@Automapped
+	private String				emailAddress;
+
+	@Optional
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				contactPhone;
+
+}
---
+++ src/main/java/acme/entities/airport_management/OperationalScope.java
@@ -0,0 +1,6 @@
+
+package acme.entities.airport_management;
+
+public enum OperationalScope {
+	INTERNATIONAL, DOMESTIC, REGIONAL
+}

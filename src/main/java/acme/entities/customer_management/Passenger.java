9c200d29646517325d2d96347055f1f5b888afc8
Antonio Luis Jiménez de la Fuente <antjimde@alum.us.es>
2025-03-09 21:43:24 +0100

    D02-S2-005 'Passenger' entity creation

---
+++ src/main/java/acme/entities/customer_management/Passenger.java
@@ -0,0 +1,53 @@
+
+package acme.entities.customer_management;
+
+import java.util.Date;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+
+public class Passenger extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@ManyToOne()
+	private Booking				booking;
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	private String				fullName;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,9}$")
+	@Automapped
+	private String				passportNumber;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birthDate;
+
+	@Optional
+	@ValidString(min = 0, max = 50)
+	@Automapped
+	private String				specialNeeds;
+
+}

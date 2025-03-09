5146c9bd4fade6fe1b35897e78735eefd4355b0b
Pablo Nieto Córdoba <pabniecor@alum.us.es>
2025-03-09 19:56:19 +0100

    feat: Leg Entity Creation

---
+++ src/main/java/acme/entities/flight_management/Leg.java
@@ -0,0 +1,90 @@
+
+package acme.entities.flight_management;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.client.helpers.MomentHelper;
+import acme.constraints.ValidLeg;
+import acme.entities.airline_operations.Aircraft;
+import acme.entities.airport_management.Airport;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@ValidLeg
+public class Leg extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{3}\\d{4}$")  // IATA code + 4 dígitos
+	@Column(unique = true, length = 7)
+	private String				flightNumber;
+
+	@Mandatory
+	@ValidMoment(max = "2201/01/01  00:00:00", past = false) //min = Current time, max = 2201/01/01  00:00:00
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledDeparture;
+
+	@Mandatory
+	@ValidMoment(min = "", max = "2201/01/01  00:00:00") //min = scheduledDeparture + 1 minute, max = 2201/01/01  00:00:00
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledArrival;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private LegStatus			status;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	@Transient
+	public Double getDuration() {
+		Integer result = MomentHelper.compare(this.scheduledArrival, this.scheduledDeparture);
+		return result.doubleValue();
+	}
+
+	// Relationships ----------------------------------------------------------
+
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Flight		flight;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airport		departureAirport;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airport		arrivalAirport;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Aircraft	aircraft;
+
+}

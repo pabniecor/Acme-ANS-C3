27f26340e9eac56630f6041002dde295d117ef91
Alejandro Vela Molina <alevelmol@alum.us.es>
2025-03-09 20:59:12 +0100

    feat: Flight Assignment Entity created

---
+++ src/main/java/acme/entities/airport_management/FlightAssignment.java
@@ -0,0 +1,59 @@
+
+package acme.entities.airport_management;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.entities.flight_management.Leg;
+import acme.realms.FlightCrewMember;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FlightAssignment extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Leg					leg;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private FlightCrewMember	flightCrew;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Duty				duty;
+
+	@Mandatory
+	@ValidMoment(past = true, min = "2000/01/01 00:00:00")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Status				currentStatus;
+
+	@Optional
+	@ValidString(min = 0, max = 255)
+	@Automapped
+	private String				remarks;
+}

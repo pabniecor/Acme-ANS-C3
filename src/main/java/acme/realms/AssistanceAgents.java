7f39495e1ce42a42eba6382b5cbdcef203fc6bc0
Darío Román Jiménez <darromjim@alum.us.es>
2025-03-10 11:30:39 +0100

    Added relations and fixed validations

--- src/main/java/acme/realms/AssistanceAgents.java
+++ src/main/java/acme/realms/AssistanceAgents.java
@@ -5,17 +5,20 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
-import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
 import acme.entities.airline_operations.Airline;
 import lombok.Getter;
 import lombok.Setter;
@@ -39,6 +42,7 @@ public class AssistanceAgents extends AbstractRole {
 
 	@Mandatory
 	@Valid
+	@ManyToOne(optional = false)
 	private Airline				airline;
 
 	@Mandatory
@@ -52,11 +56,12 @@ public class AssistanceAgents extends AbstractRole {
 	private String				briefBio;
 
 	@Optional
-	@ValidNumber(min = 0)
+	@ValidMoney(min = 0., max = Double.MAX_VALUE)
 	@Automapped
-	private Integer				salary;
+	private Money				salary;
 
 	@Optional
+	@ValidUrl
 	@Automapped
 	private String				photo;
 }

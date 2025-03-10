193bd58ffe5edfceaf2ff2d8a1e7dcf190c2de70
Antonio Luis Jiménez de la Fuente <antjimde@alum.us.es>
2025-03-10 11:47:32 +0100

    Fix: D02-S2-005: Relationship between Passenger and Booking stablished by BookingRecord entity

---
+++ src/main/java/acme/entities/customer_management/BookingRecord.java
@@ -0,0 +1,24 @@
+
+package acme.entities.customer_management;
+
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+
+public class BookingRecord extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Booking				booking;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Passenger			passenger;
+
+}

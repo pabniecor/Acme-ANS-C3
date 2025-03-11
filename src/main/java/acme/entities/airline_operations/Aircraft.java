1066dd5da82ba49033755a58bf83aa7ad8b51bd9
Alejandro Vela Molina <alevelmol@alum.us.es>
2025-03-11 18:01:38 +0100

    fix: Airline entity and sample data fixed

--- src/main/java/acme/entities/airline_operations/Aircraft.java
+++ src/main/java/acme/entities/airline_operations/Aircraft.java
@@ -3,6 +3,7 @@ package acme.entities.airline_operations;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -50,4 +51,9 @@ public class Aircraft extends AbstractEntity {
 	@ValidString(max = 255)
 	@Automapped
 	private String				details;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 }

d3d05a63183837da49a793fc291c286f273a0732
Darío Román Jiménez <darromjim@alum.us.es>
2025-03-11 19:21:19 +0100

    Added validator and modified validations at AssistanceAgents

---
+++ src/main/java/acme/constraints/AssistanceAgentsValidator.java
@@ -0,0 +1,44 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.realms.AssistanceAgents;
+
+@Validator
+public class AssistanceAgentsValidator extends AbstractValidator<ValidAssistanceAgents, AssistanceAgents> {
+
+	// Internal state ---------------------------------------------------------
+
+	//		@Autowired
+	//		private AssistanceAgentsRepository repository;
+
+	// ConstraintValidator interface ------------------------------------------
+
+	@Override
+	protected void initialise(final ValidAssistanceAgents annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final AssistanceAgents assistanceAgents, final ConstraintValidatorContext context) {
+		assert context != null;
+
+		boolean result;
+
+		{
+			String name = assistanceAgents.getIdentity().getName();
+			String surname = assistanceAgents.getIdentity().getSurname();
+
+			boolean correctIdentifier = assistanceAgents.getEmployeeCode().charAt(0) == name.charAt(0) && assistanceAgents.getEmployeeCode().charAt(1) == surname.charAt(0);
+
+			super.state(context, correctIdentifier, "identifier", "The manager's identier does not correspond to their initials");
+		}
+
+		result = !super.hasErrors(context);
+
+		return result;
+	}
+}

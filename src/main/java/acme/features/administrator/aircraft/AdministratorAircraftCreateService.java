2ff0052ae0f900d37e436af6db7e1631e27672c4
Alejandro Vela Molina <alevelmol@alum.us.es>
2025-03-25 10:06:36 +0100

    feat: Aircraft CRUD operations implemented

---
+++ src/main/java/acme/features/administrator/aircraft/AdministratorAircraftCreateService.java
@@ -0,0 +1,70 @@
+
+package acme.features.administrator.aircraft;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airline_operations.Aircraft;
+import acme.entities.airline_operations.AircraftStatus;
+import acme.entities.airline_operations.Airline;
+
+@GuiService
+public class AdministratorAircraftCreateService extends AbstractGuiService<Administrator, Aircraft> {
+
+	@Autowired
+	private AdministratorAircraftRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Aircraft aircraft;
+
+		aircraft = new Aircraft();
+		super.getBuffer().addData(aircraft);
+	}
+
+	@Override
+	public void bind(final Aircraft aircraft) {
+		super.bindObject(aircraft, "model", "registrationNumber", "capacity", "cargoWeight", "status", "details", "airline");
+	}
+
+	@Override
+	public void validate(final Aircraft aircraft) {
+		;
+	}
+
+	@Override
+	public void perform(final Aircraft aircraft) {
+		this.repository.save(aircraft);
+	}
+
+	@Override
+	public void unbind(final Aircraft aircraft) {
+		Dataset dataset;
+		Collection<Airline> airlines;
+		SelectChoices choises;
+		SelectChoices aircraftStatus;
+
+		airlines = this.repository.findAllAirlines();
+		aircraftStatus = SelectChoices.from(AircraftStatus.class, aircraft.getStatus());
+		choises = SelectChoices.from(airlines, "name", aircraft.getAirline());
+
+		dataset = super.unbindObject(aircraft, "model", "registrationNumber", "capacity", "cargoWeight", "status", "details", "airline");
+		dataset.put("airline", choises.getSelected().getKey());
+		dataset.put("airlines", choises);
+		dataset.put("status", aircraftStatus);
+
+		super.getResponse().addData(dataset);
+	}
+}

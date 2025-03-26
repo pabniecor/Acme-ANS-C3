c2bb3f480b003333eeb523ae06fad8cbbddec84e
Pablo Nieto Córdoba <pabniecor@alum.us.es>
2025-03-26 11:10:19 +0100

    feat: Airport Create and Update

---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportUpdateService.java
@@ -0,0 +1,65 @@
+
+package acme.features.administrator.airport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airport_management.Airport;
+import acme.entities.airport_management.OperationalScope;
+
+@GuiService
+public class AdministratorAirportUpdateService extends AbstractGuiService<Administrator, Airport> {
+
+	@Autowired
+	private AdministratorAirportRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Airport airport;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		airport = this.repository.findAirportById(id);
+
+		super.getBuffer().addData(airport);
+	}
+
+	@Override
+	public void bind(final Airport airport) {
+		super.bindObject(airport, "name", "iataCode", "operationalScope", "city", "country", "website", "email", "contactPhone");
+	}
+
+	@Override
+	public void validate(final Airport airport) {
+		;
+	}
+
+	@Override
+	public void perform(final Airport airport) {
+		this.repository.save(airport);
+	}
+
+	@Override
+	public void unbind(final Airport airport) {
+		assert airport != null;
+		Dataset dataset;
+		SelectChoices operationalScope;
+
+		operationalScope = SelectChoices.from(OperationalScope.class, airport.getOperationalScope());
+
+		dataset = super.unbindObject(airport, "name", "iataCode", "operationalScope", "city", "country", "website", "email", "contactPhone");
+		dataset.put("operationalScope", operationalScope);
+
+		super.getResponse().addData(dataset);
+	}
+}

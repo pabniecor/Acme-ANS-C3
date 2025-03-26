76dd617b2f6c7a5e9d446c414c8ae89918129cff
Pablo Nieto Córdoba <pabniecor@alum.us.es>
2025-03-26 13:52:27 +0100

    feat: Added Confirmation to Create and Update Airport

--- src/main/java/acme/features/administrator/airport/AdministratorAirportUpdateService.java
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportUpdateService.java
@@ -41,7 +41,10 @@ public class AdministratorAirportUpdateService extends AbstractGuiService<Admini
 
 	@Override
 	public void validate(final Airport airport) {
-		;
+		boolean confirmation;
+
+		confirmation = super.getRequest().getData("confirmation", boolean.class);
+		super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
 	}
 
 	@Override
@@ -59,6 +62,7 @@ public class AdministratorAirportUpdateService extends AbstractGuiService<Admini
 
 		dataset = super.unbindObject(airport, "name", "iataCode", "operationalScope", "city", "country", "website", "email", "contactPhone");
 		dataset.put("operationalScope", operationalScope);
+		dataset.put("confirmation", false);
 
 		super.getResponse().addData(dataset);
 	}

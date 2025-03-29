4d2556962d2af07818400d88eb0a76d4b3ec9e2b
Darío Román Jiménez <darromjim@alum.us.es>
2025-03-29 19:29:44 +0100

    Added advisor and repository for Service entity

---
+++ src/main/java/acme/components/ServiceAdvisor.java
@@ -0,0 +1,32 @@
+
+package acme.components;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import acme.entities.airport_management.Service;
+
+@ControllerAdvice
+public class ServiceAdvisor {
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ServiceRepository repository;
+
+	// Beans ------------------------------------------------------------------
+
+
+	@ModelAttribute("service")
+	public Service getAdvertisement() {
+		Service result;
+
+		try {
+			result = this.repository.findRandomService();
+		} catch (final Throwable oops) {
+			result = null;
+		}
+
+		return result;
+	}
+}
